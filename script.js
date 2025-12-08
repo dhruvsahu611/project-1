04d8714a3d8983ec3a71896e5d9ce858ae6fd86e
dhruvsahu611 <163058959+dhruvsahu611@users.noreply.github.com>
2025-12-08 11:35:44 +0530

    Add files via upload
    this is chatbot project into a website.

---
+++ script.js
@@ -0,0 +1,12 @@
+function mail(){
+    window.open("https://mail.google.com/mail/u/0/?fs=1&tf=cm&source=mailto&to=soit@rgtu.net");
+}
+function news(){
+    window.open("https://www.rgpv.ac.in/");
+}
+function support(){
+    window.open("https://www.rgpv.ac.in/Support/SupportDesk.aspx");
+}
+function chatbot(){
+    window.open("https://www.chatbase.co/chatbot-iframe/8KK6mPIl9C17BttK4-TPL");
+}
